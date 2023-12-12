0e6f10a473879025fa258c1d79c7c33ff473ae7f
Yonix49 <mohammed-981@hotmail.fr>
2023-12-12 22:06:37 +0100

    Test pour voir si le hashage fonctionne

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -1,15 +1,32 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { UserService } from './service/service.user';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function main() {
   const prisma = new PrismaClient();
+  const userService = new UserService(prisma);
 
-  app.useGlobalPipes(/* ... */);
-  // Autres configurations...
+  try {
+    const newUser = 
+    {
+      id : 1,
+      login42: 'test',
+      name : 'test',
+      status : 'en ligne',
+      image_url : 'https://cdn.intra.42.fr/users/test.jpg',
+      email: 'test@example.com',
+      password: 'password123',
+    };
 
-  await app.listen(3000);
+    const createdUser = await userService.create(newUser);
+    console.log('Utilisateur créé :', createdUser);
+  } catch (error) {
+    console.error('Erreur lors de la création de l’utilisateur :', error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-bootstrap();
+main().catch((e) => {
+  throw e;
+});
