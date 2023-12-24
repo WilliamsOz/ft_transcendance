aa8847e8a07a4b38773ab15cca6cf5a0b18644cd
Yonix49 <mohammed-981@hotmail.fr>
2023-12-24 15:44:02 +0100

    modification sur le main

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -1,31 +1,8 @@
-import { PrismaClient } from '@prisma/client';
-import * as bcrypt from 'bcrypt';
-import { UserService } from './service/service.user';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
-async function main() {
-  const prisma = new PrismaClient();
-  const userService = new UserService(prisma);
-
-  try {
-    const newUser = {
-      id: 2,
-      login42: 'momo',
-      name: 'babar',
-      status: 'en ligne',
-      image_url: 'https://cdn.intra.42.fr/users/babar.jpg',
-      email: 'babar@example.com',
-      password: 'lol321312',
-    };
-
-    const createdUser = await userService.create(newUser);
-    console.log('Utilisateur créé :', createdUser);
-  } catch (error) {
-    console.error('Erreur lors de la création de l’utilisateur :', error);
-  } finally {
-    await prisma.$disconnect();
-  }
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
-
-main().catch((e) => {
-  throw e;
-});
+bootstrap();
